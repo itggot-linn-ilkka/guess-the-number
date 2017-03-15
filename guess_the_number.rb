a189d904bb642c690c451b8ae720a58cbfd4bf68
itggot-linn-ilkka <linn.ilkka@itggot.se>
2017-03-15 10:01:01 +0100

    lagt till sleep

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -7,13 +7,14 @@ puts "select difficult"
 difficult = gets.to_i
 secret_number=rand 1..difficult
 
-  puts "guess the secret number between 1..difficut"
+  puts "guess the secret number between 1..difficult"
 
 while running
   number = gets.to_i
-  i + 1
+  i += 1
   if number==secret_number
   puts "good job"
+  print "your guess " + i = i.to_s + " times"
   break
 elsif number > secret_number
   puts "try again"
